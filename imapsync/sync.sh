cd6cf4b156b221b077f976b4816f304157821611
Dennis Schulz <d.schulz@ablida.de>
2016-12-18 09:20:02 +0100

    rainloop experiment. actually only a blank page :(

---
+++ imapsync/sync.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+IMAPSYNC=/usr/bin/imapsync
+
+sleep 30
+
+while true; do
+  $IMAPSYNC \
+    --host1 imap.1und1.de \
+    --user1 dennis@port42.org \
+    --password1 'TzTGMkMmVtMREza8mO!hK' \
+    --authmech1 PLAIN --tls1 \
+    --host2 mail \
+    --user2 dennis@port42.org \
+    --password2 'test1234' \
+    --authmech2 PLAIN --tls2
+  sleep 3600
+done
